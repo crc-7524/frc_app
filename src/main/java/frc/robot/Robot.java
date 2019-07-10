2cb2bdb5356d5ef6026fa48b518ad3820975140b
Neuron <liziyuan123456@yeah.net>
2019-07-11 04:14:20 +0800

    try new speed

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -22,63 +22,101 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
  * directory.
  */
 public class Robot extends TimedRobot {
-  //private final DifferentialDrive m_robotDrive
-  //    = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
-  private final SpeedControllerGroup m_left = new SpeedControllerGroup(new PWMVictorSPX(0), new PWMVictorSPX(1));
-  private final SpeedControllerGroup m_right = new SpeedControllerGroup(new PWMVictorSPX(2), new PWMVictorSPX(3));
-  private final DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
-  private final Joystick m_stick = new Joystick(0);
-  private final Timer m_timer = new Timer();
-
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-  }
-
-  /**
-   * This function is run once each time the robot enters autonomous mode.
-   */
-  @Override
-  public void autonomousInit() {
-    m_timer.reset();
-    m_timer.start();
-  }
-
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    // Drive for 2 seconds
-    if (m_timer.get() < 2.0) {
-      m_drive.arcadeDrive(0.5, 0.0); // drive forwards half speed
-    } else {
-      m_drive.stopMotor(); // stop robot
+    //private final DifferentialDrive m_robotDrive
+    //    = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+    private final SpeedControllerGroup m_left = new SpeedControllerGroup(new PWMVictorSPX(0), new PWMVictorSPX(1));
+    private final SpeedControllerGroup m_right = new SpeedControllerGroup(new PWMVictorSPX(2), new PWMVictorSPX(3));
+    private final DifferentialDrive m_drive = new DifferentialDrive(m_left, m_right);
+    private final Joystick m_stick = new Joystick(0);
+    private final Timer m_timer = new Timer();
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    @Override
+    public void robotInit() {
     }
+
+    /**
+     * This function is run once each time the robot enters autonomous mode.
+     */
+    @Override
+    public void autonomousInit() {
+        m_timer.reset();
+        m_timer.start();
+    }
+
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        // Drive for 2 seconds
+        if (m_timer.get() < 2.0) {
+            m_drive.arcadeDrive(0.5, 0.0); // drive forwards half speed
+        } else {
+            m_drive.stopMotor(); // stop robot
+        }
+    }
+
+    /**
+     * This function is called once each time the robot enters teleoperated mode.
+     */
+    @Override
+    public void teleopInit() {
+    }
+
+    /**
+     * This function is called periodically during teleoperated mode.
+     */
+    @Override
+    public void teleopPeriodic() {
+      double x = m_stick.getX();
+      double y = m_stick.getY();
+      System.out.println("X: "+x);
+      System.out.println("y: "+y);
+      m_drive.arcadeDrive(-m_stick.getY() / 2, m_stick.getX() / 2);
+      /*double[] speed = getSpeed(m_stick.getX(), m_stick.getY());
+      System.out.println("output x: "+speed[0]);
+      System.out.println("output y: "+speed[1]);
+      m_left.set(-speed[0]/4);
+      m_right.set(-speed[1]/4);*/
+    }
+
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+    }
+
+    private static double[] getSpeed(double x, double y) {
+      double speedL, speedR;
+      if (x != 0 && y != 0) {
+          double ax = Math.abs(x);
+          double ay = Math.abs(y);
+          int sign = y > 0 ? 1 : -1;
+          double r = sign * (Math.toDegrees(Math.atan2(ay, ax)) - 45) / 45;
+          double s = sign * (ax >= ay ? (Math.sqrt(2) * ay) : (Math.sqrt(2) * ax));
+          if (x * y > 0) {
+              speedL = r;
+              speedR = -s;
+          } else {
+              speedL = s;
+              speedR = -r;
+          }
+      } else if (x == 0 && y == 0) {
+          speedL = 0;
+          speedR = 0;
+      } else if (x == 0) {
+          speedL = y;
+          speedR = -y;
+      } else {
+          speedL = -x;
+          speedR = -x;
+      }
+      double[] speed = {speedL, speedR};
+      return speed;
   }
 
-  /**
-   * This function is called once each time the robot enters teleoperated mode.
-   */
-  @Override
-  public void teleopInit() {
-  }
-
-  /**
-   * This function is called periodically during teleoperated mode.
-   */
-  @Override
-  public void teleopPeriodic() {
-    m_drive.arcadeDrive(-m_stick.getY()/2, m_stick.getX()/2);
-  }
-
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
 }
